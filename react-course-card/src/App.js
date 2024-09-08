7d562d8d8668d8b0c2852cc53261bd4cd79f6df2
Richard Yee <richardyee@192-168-1-2.tpgi.com.au>
2024-09-08 14:46:50 +1000

    make 3 cards stateful

--- react-course-card/src/App.js
+++ react-course-card/src/App.js
@@ -1,7 +1,34 @@
-import CourseCard from "./CourseCard";
+import CourseCard from "./components/courseCard/CourseCard";
 
 function App() {
-  return <CourseCard />;
+  const style = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "0",
+  };
+  return (
+    <div className="App" style={style}>
+      <CourseCard
+        title="ES001 HTML Index"
+        difficulty="Elementary"
+        icon="integration_instructions"
+        cardTopColor="rgb(13, 67, 76)"
+      />
+      <CourseCard
+        title="ES002 CSS Styles"
+        difficulty="Intermediate"
+        icon="format_paint"
+        cardTopColor="rgb(106, 90, 205)"
+      />
+      <CourseCard
+        title="ES003 JavaScript"
+        difficulty="Advanced"
+        icon="account_tree"
+        cardTopColor="rgb(70, 130, 180)"
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ react-course-card/src/components/courseCard/CourseCard.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import "./CourseCard.css";
+
+const CourseCard = (props) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [comment, setComment] = useState("");
+  const [apply, setApply] = useState(0);
+
+  const handleVisible = () => {
+    setIsVisible(!isVisible);
+    setIsSubmitted(false);
+  };
+
+  const handleSubmit = () => {
+    if (comment.trim() === "") {
+      alert("Please add your comment.");
+      return;
+    }
+    setIsSubmitted(!isSubmitted);
+    setComment("");
+  };
+
+  const handleComment = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleApply = () => {
+    setApply(apply + 1);
+  };
+
+  const changeFooterColor = () => {
+    const difficulty = props.difficulty;
+    if (difficulty === "Elementary") {
+      return "hsl(0,100%,80%)";
+    } else if (difficulty === "Intermediate") {
+      return "hsl(0,50%,40%)";
+    } else {
+      return "hsl(0, 20%, 20%)";
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className="card-top" style={{ backgroundColor: props.cardTopColor }}>
+        <span class="card-top-icon material-symbols-outlined">
+          {props.icon}
+        </span>
+        <div className="card-top-content">
+          <h5 className="card-top-content-online">ONLINE</h5>
+          <h6 className="card-top-content-applies">{`${apply} Applied`}</h6>
+        </div>
+      </div>
+      <div className="card-main">
+        <h2 className="card-main-title">{props.title}</h2>
+        <button className="card-main-button" onClick={handleApply}>
+          {props.difficulty === "Elementary" ? "Study Now!" : "Apply Now"}
+        </button>
+        <button className="card-main-button" onClick={handleVisible}>
+          Comment
+        </button>
+        <div className="card-main-bottom">
+          <h3 className="card-main-bottom-price">$450.00</h3>
+          <div className="card-main-bottom-context">
+            <p className="card-main-bottom-context-language">EN</p>
+            <p className="card-main-bottom-context-time">8h 00m</p>
+          </div>
+        </div>
+      </div>
+      <div
+        className="card-footer"
+        style={{ backgroundColor: changeFooterColor() }}
+      >
+        <p className="card-footer-difficulty">{props.difficulty}</p>
+      </div>
+      {isVisible && (
+        <div className="card-comment-visible">
+          <textarea
+            className="card-comment-content"
+            placeholder="No more than 100 words"
+            value={comment}
+            onChange={handleComment}
+          ></textarea>
+          <button
+            className="card-comment-submit"
+            onClick={handleSubmit}
+            disabled={isSubmitted}
+          >
+            Submit
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CourseCard;
